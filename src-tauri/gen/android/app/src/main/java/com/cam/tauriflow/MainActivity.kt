f362d7a8b7ae1f01df8430662e7920a163c66abe
Cameron Kirk <cam.kirks.email@gmail.com>
2025-08-14 21:18:12 +1000

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/cam/tauriflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cam.tauriflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
