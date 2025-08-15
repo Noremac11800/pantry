1f991b88356466bee456d750c043156ad6094873
Cameron Kirk <cam.kirks.email@gmail.com>
2025-08-15 23:07:41 +1000

    Add Tauri os plugin to mobile build steps too

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -42,6 +42,7 @@ pub fn run() {
     // Mobile build
     #[cfg(any(target_os = "android", target_os = "ios"))]
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
