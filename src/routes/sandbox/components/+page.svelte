b4505440c87f0d5d2683cecc6ac4f9eea44d2106
Noremac11400 <cam.kirks.email@gmail.com>
2025-09-22 04:33:36 +0100

    Update button styles to have disabled appearance

--- src/routes/sandbox/components/+page.svelte
+++ src/routes/sandbox/components/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-  import { Breadcrumb, BreadcrumbItem, Button } from "flowbite-svelte";
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    Checkbox,
+  } from "flowbite-svelte";
   import { t } from "svelte-i18n";
+
+  let areButtonsDisabled = $state(false);
 </script>
 
 <Breadcrumb>
@@ -16,10 +23,28 @@
 </Breadcrumb>
 
 <h1>{$t("page-title.components")}</h1>
-<Button class="brand-solid-button">Brand Solid Button</Button>
-<Button class="brand-outline-button">Brand Outline Button</Button>
-<Button class="brand-transparent-button">Brand Transparent Button</Button>
-<Button class="neutral-solid-button">Neutral Solid Button</Button>
-<Button class="neutral-outline-button">Neutral Outline Button</Button>
-<Button class="neutral-transparent-button">Neutral Transparent Button</Button>
-<Button class="status-danger-button">Status Danger Button</Button>
+<div class="flex items-center gap-2">
+  <Checkbox bind:checked={areButtonsDisabled} />
+  <span>Disable buttons</span>
+</div>
+<Button class="brand-solid-button" disabled={areButtonsDisabled}
+  >Brand Solid Button</Button
+>
+<Button class="brand-outline-button" disabled={areButtonsDisabled}
+  >Brand Outline Button</Button
+>
+<Button class="brand-transparent-button" disabled={areButtonsDisabled}
+  >Brand Transparent Button</Button
+>
+<Button class="neutral-solid-button" disabled={areButtonsDisabled}
+  >Neutral Solid Button</Button
+>
+<Button class="neutral-outline-button" disabled={areButtonsDisabled}
+  >Neutral Outline Button</Button
+>
+<Button class="neutral-transparent-button" disabled={areButtonsDisabled}
+  >Neutral Transparent Button</Button
+>
+<Button class="status-danger-button" disabled={areButtonsDisabled}
+  >Status Danger Button</Button
+>
